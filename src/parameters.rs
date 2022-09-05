37fc89dcbc78f7b9df827458294fa20bfd7e2e27
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2022-09-05 15:56:49 +0500

    initial commit

---
+++ src/parameters.rs
@@ -0,0 +1,22 @@
+// -*- mode: rust; -*-
+//
+// This file is part of dalek-frost.
+// Copyright (c) 2020 isis lovecruft
+// See LICENSE for licensing information.
+//
+// Authors:
+// - isis agora lovecruft <isis@patternsinthevoid.net>
+
+//! Configurable parameters for an instance of a FROST signing protocol.
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+/// The configuration parameters for conducting the process of creating a
+/// threshold signature.
+#[derive(Copy, Clone, Debug, Eq, Hash, BorshDeserialize, BorshSerialize, PartialEq)]
+pub struct Parameters {
+    /// The number of participants in the scheme.
+    pub n: u32,
+    /// The threshold required for a successful signature.
+    pub t: u32,
+}
